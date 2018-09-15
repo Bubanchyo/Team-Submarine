d520c33ffb1ebf6cd12e92e212a00a914dc1ecfe
jskim2356 <scitmaster.jsk@gmail.com>
2018-09-15 19:49:33 +0900

    photo.jsp 만들어야

---
+++ JourneyDiary_0913/src/main/java/journey/diary/test/controllers/PhotoController.java
@@ -0,0 +1,46 @@
+package journey.diary.test.controllers;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import journey.diary.test.DAO.AlbumMapper;
+import journey.diary.test.DAO.MemberMapper;
+import journey.diary.test.VO.Album;
+import journey.diary.test.VO.Member;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class PhotoController {
+	
+	@Autowired
+	SqlSession sqlSession;
+		
+		@RequestMapping(value = "/photoList", method = RequestMethod.GET)
+		public String photoList(Locale locale, Model model, HttpSession session, int albumno) {
+			
+
+			//photoList불러오기
+			
+			
+
+			return "photo/photoList";
+
+		}
+		
+
+		
+	}
